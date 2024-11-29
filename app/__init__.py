f38d8b0d9c3e94852e51c61f1f380e33622a7835
Skeyling1 <skeyling1@gmail.com>
2024-11-29 09:05:09 +0300

    rrr

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+# настройки фласка
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #это имя файла базы данных, который будет создан в корневом каталоге проекта
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app) #для безопасного хэширования паролей
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'  # Модуль будет перенаправлять пользователя на маршрут, который мы указываем (на авторизацию)
+
+from app import routes
